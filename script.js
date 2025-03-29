ecf8d3793e3adce8df391386e0d56f4d8cf6fbf1
SaraShalabi1752000 <134524473+SaraShalabi23@users.noreply.github.com>
2025-03-29 17:41:45 +0300

    ظظظظ

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.getElementById('translationForm').addEventListener('submit', function (
 
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+
     const sourceLanguage = document.getElementById('sourceLanguage').value;
     const targetLanguage = document.getElementById('targetLanguage').value;
     const fileInput = document.getElementById('file').files[0];
@@ -10,6 +12,7 @@ document.getElementById('translationForm').addEventListener('submit', function (
     console.log('Details submitted:');
     console.log('Name:', name);
     console.log('Email:', email);
+    console.log('phone:', phone);
     console.log('Source Language:', sourceLanguage);
     console.log('Target Language:', targetLanguage);
 
@@ -19,3 +22,37 @@ document.getElementById('translationForm').addEventListener('submit', function (
 
     alert('הפרטים נשלחו בהצלחה! נשלח מייל בהמשך.');
 });
+
+//for animation : 
+const submitButton = document.querySelector('button');
+
+submitButton.addEventListener('click', function () {
+    submitButton.classList.add('clicked');
+    setTimeout(() => {
+        submitButton.classList.remove('clicked');
+    }, 300);
+});
+
+//for the popup : 
+document.getElementById('translationForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const sourceLanguage = document.getElementById('sourceLanguage').value;
+    const targetLanguage = document.getElementById('targetLanguage').value;
+    const fileInput = document.getElementById('file').files[0];
+
+    if (!name || !email || !sourceLanguage || !targetLanguage || !fileInput) {
+        alert("אנא מלא את כל השדות");
+        return;
+    }
+
+    // פתח את הפופאפ
+    document.getElementById('popupOverlay').style.display = 'flex';
+});
+
+// פונקציית סגירה
+function closePopup() {
+    document.getElementById('popupOverlay').style.display = 'none';
+}
